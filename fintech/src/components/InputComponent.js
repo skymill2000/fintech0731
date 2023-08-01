6108b16e0980ef8da05684eee011ea6d93184e3f
유관우 <skymill2000@gmail.com>
2023-08-01 14:44:48 +0900

    Lecture(이벤트 처리): 이벤트 바인딩

--- fintech/src/components/InputComponent.js
+++ fintech/src/components/InputComponent.js
@@ -1,10 +1,18 @@
 import { useState } from "react";
 
 const InputComponent = () => {
+  const handleClick = () => {
+    console.log("클릭");
+  };
+
+  const handleChange = () => {
+    console.log("변경");
+  };
+
   return (
     <div>
-      <input></input>
-      <button>입력</button>
+      <input onChange={handleChange}></input>
+      <button onClick={handleClick}>입력</button>
     </div>
   );
 };
