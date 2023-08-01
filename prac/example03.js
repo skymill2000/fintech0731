198e54bfa7426eda368601f2d419ba70686a3212
유관우 <skymill2000@gmail.com>
2023-08-01 11:46:35 +0900

    Lecture(사칙연산구현): 사칙연산 구현

--- prac/example03.js
+++ prac/example03.js
@@ -2,9 +2,21 @@ function plus(p1, p2) {
   console.log(p1 + p2);
 }
 
-const plusArrowFunc = (p1, p2) => {
-  console.log(p1 + p2);
+const minus = (p1, p2) => {
+  console.log(p1 - p2);
+};
+
+const mul = (p1, p2) => {
+  console.log(p1 * p2);
+};
+
+const div = (p1, p2) => {
+  console.log(p1 / p2);
 };
 
 plus(5, 10);
-plusArrowFunc(5, 10);
+minus(5, 10);
+mul(5, 10);
+div(5, 10);
+
+//사칙연산 나머지 arrow func 구현
