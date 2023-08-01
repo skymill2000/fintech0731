96c7a0868072cdee1ece40c85c05667392a7a6c8
유관우 <skymill2000@gmail.com>
2023-08-01 13:50:25 +0900

    Lecture(Component 와 Pros): Welcome 컴포넌트 구현

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,21 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const Welcome = () => {
+  return (
+    <div>
+      <p>안녕하세요 홍길동 님!</p>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>안녕하세요</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome></Welcome>
     </div>
   );
 }
