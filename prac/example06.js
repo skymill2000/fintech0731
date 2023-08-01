94c21075d03167a4efe7f70f1e96248eceab2105
유관우 <skymill2000@gmail.com>
2023-08-01 13:15:42 +0900

    Lecture(구조분해할당): 구조분해 할당

---
+++ prac/example06.js
@@ -0,0 +1,19 @@
+let car = {
+  name: "sonata",
+  hp: 1000,
+  start: function () {
+    console.log("engine stop");
+  },
+  stop: function () {
+    console.log("engine stop");
+  },
+};
+
+let carname = car.name;
+let carhp = car.hp;
+
+console.log(carname, carhp);
+
+let { name, hp } = car;
+
+console.log(name, hp);
