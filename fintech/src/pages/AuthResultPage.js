4640ec016811d88b0e87dd390c26bf8741f8d518
유관우 <skymill2000@gmail.com>
2023-08-02 14:41:42 +0900

    Lecture(Proxy 설정 및 토큰 요청 완성):  서버 재시작 필요

--- fintech/src/pages/AuthResultPage.js
+++ fintech/src/pages/AuthResultPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import AppHeader from "../components/common/AppHeader";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
-
+import axios from "axios";
 const AuthResultPage = () => {
   const queryParams = useLocation().search;
   console.log(queryParams);
@@ -14,14 +14,22 @@ const AuthResultPage = () => {
     //axios 요청 만들기
     let requestOption = {
       //메뉴얼보고작성
-      url: "토큰요청",
+      url: "/oauth/2.0/token",
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
       },
-      data: "",
+      data: {
+        code: authCode,
+        client_id: "9c785a50-ac39-4dac-af61-eb82eb80e139",
+        client_secret: "93b9698e-14a8-4462-9f76-6fe06e0250de",
+        redirect_uri: "http://localhost:3000/authResult",
+        grant_type: "authorization_code",
+      },
     };
-    axios(requestOption).then(() => {});
+    axios(requestOption).then((response) => {
+      console.log(response);
+    });
   };
 
   return (
