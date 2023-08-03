a42cb4718556e8ce3b3f05e5b370cb1112da740b
유관우 <skymill2000@gmail.com>
2023-08-03 15:27:57 +0900

    Lecture(출금이체): 출금이체 기능 구현 완료

--- fintech/src/components/withdraw/ModalCard.js
+++ fintech/src/components/withdraw/ModalCard.js
@@ -45,6 +45,44 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
     // axios option으로 요청을 작성하기 <- api 요청
     // application/json 은 데이터를 어떻게 전송?
     // 결과를 로그로 작성
+
+    const accessToken = localStorage.getItem("accessToken");
+
+    const data = {
+      bank_tran_id: genTransId(),
+      cntr_account_type: "N",
+      cntr_account_num: "100000000001",
+      dps_print_content: "쇼핑몰환불",
+      fintech_use_num: fintechUseNo,
+      wd_print_content: "오픈뱅킹출금",
+      tran_amt: amount,
+      tran_dtime: "20230802130000",
+      req_client_name: "홍길동",
+      req_client_fintech_use_num: fintechUseNo,
+      req_client_num: "HONGGILDONG1234",
+      transfer_purpose: "ST",
+      recv_client_name: "홍길동",
+      recv_client_bank_code: "097",
+      recv_client_account_num: "100000000001",
+    };
+
+    console.log(data);
+    // tran_amt, fintech_use_num, req_client_fintech_use_num, bank_tran_id 고정값 사용 금지 나머지는 고정값으로
+    // axios option으로 요청을 작성하기 <- api 요청
+    const option = {
+      method: "POST",
+      url: "/v2.0/transfer/withdraw/fin_num",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: data,
+    };
+    axios(option).then(({ data }) => {
+      console.log(data);
+      if (data.rsp_code === "A0000") {
+        alert("출금 완료");
+      }
+    });
   };
 
   const deposit = () => {
