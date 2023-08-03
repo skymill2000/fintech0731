27645297b8433cf269d645f2752cacdb4c09ca2c
유관우 <skymill2000@gmail.com>
2023-08-03 11:44:03 +0900

    Lecture(QrCodeReader 추가 완료): 추가 완료

--- fintech/src/pages/QrReaderPage.js
+++ fintech/src/pages/QrReaderPage.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import AppHeader from "../components/common/AppHeader";
+import QrCodeReader, { QRCode } from "react-qrcode-reader";
 
 const QrReaderPage = () => {
+  const [val, setVal] = useState("");
+
   return (
     <div>
       <AppHeader title={"qr 리더"}></AppHeader>
+      <QrCodeReader delay={100} width={600} height={500} action={setVal} />
     </div>
   );
 };
