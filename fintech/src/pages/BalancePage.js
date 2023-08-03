a1f92dce6730670dccdac60cc2ee9241734534b6
유관우 <skymill2000@gmail.com>
2023-08-03 10:43:47 +0900

    Lecture(잔액조회): 디자인 카드 적용

--- fintech/src/pages/BalancePage.js
+++ fintech/src/pages/BalancePage.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import AppHeader from "../components/common/AppHeader";
 import queryString from "query-string";
+import axios from "axios";
+import BalanceCard from "../components/balance/BalanceCard";
 
 const BalancePage = () => {
   let accessToken = "";
@@ -34,13 +36,50 @@ const BalancePage = () => {
     userSeqNo = localStorage.getItem("userSeqNo");
     getBalance();
   }, []);
+  function getCurrentDateTime() {
+    const now = new Date();
 
-  const getBalance = () => {};
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const day = String(now.getDate()).padStart(2, "0");
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+
+    const currentDateTime = `${year}${month}${day}${hours}${minutes}${seconds}`;
+    return currentDateTime;
+  }
+
+  const getBalance = () => {
+    const sendObj = {
+      bank_tran_id: genTrasId(),
+      fintech_use_num: fintechUseNum,
+      tran_dtime: getCurrentDateTime(),
+    };
+
+    const option = {
+      method: "GET",
+      url: "v2.0/account/balance/fin_num",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: sendObj,
+    };
+
+    axios(option).then(({ data }) => {
+      setBalance(data);
+    });
+  };
 
   return (
     <div>
       <AppHeader title="잔액조회"></AppHeader>
-      {balance}
+      <BalanceCard
+        bankName={balance.bank_name}
+        fintechNo={balance.fintech_use_num}
+        balance={balance.balance_amt}
+      ></BalanceCard>
     </div>
   );
 };
