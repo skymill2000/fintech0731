e04fee6ba8c3e4b6cec35638279c3d9e97102955
유관우 <skymill2000@gmail.com>
2023-08-03 14:36:26 +0900

    Lecture(QRcode): 위치 변경

--- fintech/src/pages/QrCodePage.js
+++ fintech/src/pages/QrCodePage.js
@@ -3,6 +3,13 @@ import AppHeader from "../components/common/AppHeader";
 import { QRCodeSVG } from "qrcode.react";
 import queryString from "query-string";
 import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+const QRBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+`;
 
 const QrCodePage = () => {
   const queryParams = useLocation().search;
@@ -12,7 +19,9 @@ const QrCodePage = () => {
   return (
     <div>
       <AppHeader title={"QR 코드"} />
-      <QRCodeSVG value={fintechUseNum} size={256} />
+      <QRBlock>
+        <QRCodeSVG value={fintechUseNum} size={256} />
+      </QRBlock>
     </div>
   );
 };
