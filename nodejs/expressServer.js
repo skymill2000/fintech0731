8f1d035d990045ad876b1c0e180fba67a5a1bd3a
유관우 <skymill2000@gmail.com>
2023-08-04 13:26:42 +0900

    Lecture(Post / GET): 데이터 전송 확인

--- nodejs/expressServer.js
+++ nodejs/expressServer.js
@@ -1,8 +1,24 @@
 var express = require("express");
 var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+app.get("/test", function (req, res) {
+  res.send("test page 입니다.");
+});
+
+app.get("/req", function (req, res) {
+  console.log(req.query.test);
+  res.send("hello world");
+});
+
+app.post("/req", function (req, res) {
+  console.log(req.body);
   res.send("hello world");
 });
 
