13d386c36535a2733662b09813b2ff2f2d59d332
유관우 <skymill2000@gmail.com>
2023-08-04 16:15:29 +0900

    Lecture(사용자 정보 조회): 서버에서 사용자 정보 조회 실습

--- nodejs/CryptoTest.js
+++ nodejs/CryptoTest.js
@@ -15,8 +15,9 @@ const AESEncrypt = (plainTxt) => {
   console.log(encrypted);
 };
 
-AESEncrypt("1234");
-// AESDecrypt("");
+AESEncrypt(
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMTAwMDM0NzM2Iiwic2NvcGUiOlsiaW5xdWlyeSIsImxvZ2luIiwidHJhbnNmZXIiXSwiaXNzIjoiaHR0cHM6Ly93d3cub3BlbmJhbmtpbmcub3Iua3IiLCJleHAiOjE2OTg3MzI2NjgsImp0aSI6IjdjNmQ3YTU1LTViMDYtNDVkYi1hNTk5LTg1MDQ2Y2FjZDNmNSJ9.UHVSxOs1aQ1pNkG_VffBr0_KEKrn7o-rj-VZA-A8EmY"
+);
 
 const AESDecrypt = (plainTxt) => {
   const algorithm = "aes-256-cbc";
@@ -28,4 +29,6 @@ const AESDecrypt = (plainTxt) => {
   console.log(decrypted);
 };
 
-AESDecrypt("BHH3zWvc/D+AuxB1jGOZ8doMqLEcNk1KKUjYLZnZeHU=");
+AESDecrypt(
+  "Oc//x6KviGTY48VIIrc5hPnmxqh5mFNx0zJwxdn+v8JOzpmKJoeON9zfCKO+fiIyNKiOh8ZKgfbQhJOyCUyhmtmB2KNzsD6d9DvHqYt7niRP5VLqu/eEJ8+LkQSAsiS1TH7fSosOGyFmYH1Wrdqg1x/d2k35OCxFJxHmSuzdq9GJIFj20J3BkKrpPUGpbxX3F0z97dEVlLbMGDNnC7gMIdxZXplg3G1NaqCRKue9PRiLtbM+jLdM9SWPF2iMASph76AG2djjluF5VxSgl8LvJISm1TAblh5TneiXTyMBYV9CY5WZ8HyJwauQ8u+Yft5g4p5X7yFZ/Er5GJgXNDQF9+/DKLNsI+oVz7s4QJwZLuRdIN6XauoK14n3Xamy1lHm17J5cYwkUJK+s6cT0P7Sdg=="
+);
--- nodejs/expressServer.js
+++ nodejs/expressServer.js
@@ -76,8 +76,31 @@ app.post("/login", function (req, res) {
   });
 });
 
+app.get("/account", auth, (req, res) => {
+  let { userId } = req.decoded;
+  const sql = "SELECT * FROM user WHERE id = ?";
+  connection.query(sql, [userId], function (err, result) {
+    const accesstoken = AESDecrypt(result[0].accesstoken);
+    const userSeqNo = result[0].userseqno;
+    console.log(accesstoken);
+
+    //axios 요청 작성
+  });
+});
+
 const hash = (input) => {
   return crypto.createHmac("sha256", secret).update(input).digest("hex");
+};
+
+const AESDecrypt = (plainTxt) => {
+  const algorithm = "aes-256-cbc";
+  const key = "tDAArT4tgoJra4AVYYUgt9Nvb9aImrTm";
+  const iv = "oNYgvfAAoAUb9mmD";
+  const cipher = crypto.createDecipheriv(algorithm, key, iv);
+  let decrypted = cipher.update(plainTxt, "base64", "utf8");
+  decrypted += cipher.final("utf-8");
+  console.log(decrypted);
+  return decrypted;
 };
 
 app.listen(4000);
